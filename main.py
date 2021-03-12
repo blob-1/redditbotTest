1c9ffc027647baa379783ba43ca763be59292382
blob-1 <63095628+blob-1@users.noreply.github.com>
2021-03-12 12:59:25 +0100

    forgot to remove the quit() for debug...

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ parser.add_argument("--get", type=eval, default = True, help="fetch data")
 args = parser.parse_args()
 
 if args.get:
-	quit()
 	start = dt.datetime(2017, 1, 1)
 	end   = dt.datetime(2017, 1, 3)
 
@@ -17,8 +16,6 @@ if args.get:
 
 	Getter.save("test.json")
 else:
-	print("2")
-	quit()
 	Getter = RedditGetter.loadData("test.json")
 	
 Getter.Show()
